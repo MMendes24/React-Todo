4d9bcfc5080da88945352321472e2306b39f2516
MMendes24 <mmbusiness1@protonmail.com>
2020-08-03 14:53:10 -0400

    Successfully mapping over dummy data and rendering it to the screen. Skeleton of the app complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import TodoList from './components/TodoList'
 
 
-const listItem = []
+const listItems = [
+  {
+    name: "Create a todo list",
+    id: 1,
+    cleared: false
+  },
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -9,13 +16,14 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super()
-    this.state = { listItem }
+    this.state = { listItems } // props destructuring, similar to what you do with Hooks (compare later)
 }
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList listItems={this.state.listItems} />
       </div>
     );
   }
