3e311a270e35ae7f5140985abf24c33e75d15823
MMendes24 <mmbusiness1@protonmail.com>
2020-08-20 15:01:44 -0400

    Test commit.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  //test test test
   constructor() {
     super()
     this.state = { listItems } // props destructuring, similar to what you do with Hooks (compare later)
